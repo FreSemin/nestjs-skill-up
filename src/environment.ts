85a401da7e873b13affbf02bc37f92a46e7a2734
Andrei Filinovich <fresemin@gmail.com>
2025-06-29 19:34:56 +0300

    feat: settings for app port and host

--- src/environment.ts
+++ src/environment.ts
@@ -4,6 +4,10 @@ import * as env from 'env-var';
 config();
 
 export const environment = {
+  app: {
+    host: env.get('APP_HOST').default('http://127.0.0.1').asString(),
+    port: env.get('APP_PORT').default(3000).asPortNumber(),
+  },
   database: {
     host: env.get('POSTGRES_HOST').required().asString(),
     port: env.get('POSTGRES_PORT').required().asPortNumber(),
